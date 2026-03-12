37b591b80fb648611d3e0eed75056b97c98135b8
elvinr124-dev <elvinr124@gmail.com>
2026-03-12 17:05:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path"; // 1. Add this import
 import { viteSingleFile } from "vite-plugin-singlefile";
 
 export default defineConfig({
-  base: "./", // Add this line here
-  plugins: [react(), viteSingleFile()],
+  base: "./", 
+  plugins: [
+    react(), 
+    viteSingleFile()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // 2. Add this alias for Shadcn
+    },
+  },
 });
